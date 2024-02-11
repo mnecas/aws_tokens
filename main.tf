dc88ee4bb539e0bec2280a56c8a71456a49f8cb1
Martin Necas <mnecas@redhat.com>
2024-02-11 20:45:29 +0100

    Fix required_providers version

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
     }
   }
 }
